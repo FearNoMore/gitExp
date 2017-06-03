1f8ab5a4c002ec57474f25be333eca1fbd62ae25
Rafael Moreno <spelljammer83@gmail.com>
2017-06-03 14:22:50 +0300

    added Lorem

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
 	<h1>Git Lesson2</h1>
 	<p>Новое изменение файла index.php</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga fugit, itaque nobis dolore deserunt laborum eaque amet facere voluptatum culpa fugiat, repellendus veniam unde, ipsa ex voluptas incidunt ipsum adipisci?</p>
 </body>
 </html>
