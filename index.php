9e819256ec4259725b66e081cb423fbc8ac455e1
Rafael Moreno <spelljammer83@gmail.com>
2017-06-03 14:24:40 +0300

    Deleted all body, Adde new Lorem

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>Git Lesson2</h1>
-	<p>Новое изменение файла index.php</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga fugit, itaque nobis dolore deserunt laborum eaque amet facere voluptatum culpa fugiat, repellendus veniam unde, ipsa ex voluptas incidunt ipsum adipisci?</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum perspiciatis, voluptatum quam voluptas omnis incidunt laboriosam, alias dolorem. Tempore qui temporibus, deserunt eius reiciendis necessitatibus harum, sed voluptates labore nihil!</p>
 </body>
 </html>
